12ae8c448cc4cf5ce30a36bf6441ba9172347f29
seanmark89 <seanmark89@gmail.com>
2020-01-25 19:41:30 -0600

    Update Card.java

--- Card.java
+++ Card.java
@@ -46,6 +46,25 @@ public class Card{
         return suits[this.suit - 1];
 
     }//getter for Suit Name
-
+    
+    public void setSuit(int suit) {
+        
+        if (suit < 1 || suit > 4){
+            throw new IllegalArgumentException("Suit must be between 1-4");
+        }
+        else {
+            this.suit = suit;
+        }
+        
+    }//setter for Suit
+    
+    public void setNum(int num) {
+        if (num < 1 || num > 13) {
+            throw new IllegalArgumentException("Rank must be 1 - 13");
+        }
+        else {
+            this.num = num;
+        }
+    }
 
 }//class Card
