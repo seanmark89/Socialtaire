fa8fa9e88a1129cd9e25121e24a817a1641c83a3
v-milen <milen014@umn.edu>
2020-01-26 13:22:22 -0600

    test

--- Graphics.java
+++ Graphics.java
@@ -6,3 +6,17 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+public class GUI extends Application {
+
+    @Overide
+    public void start(Stage stage) {
+        initUI(stage);
+
+        stage.setFullScreen(true);  // full screen
+        stage.initStyle(StageStyle.DECORATED);  // title bar & minimize / maximize / close buttons
+        stage.setTitle("Socialtaire");
+    }
+
+    stage.show();
+
+}
