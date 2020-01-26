265e32ae430dfb4f6857a841e3b65dfc736e960c
oseli001 <60305674+oseli001@users.noreply.github.com>
2020-01-26 09:47:08 -0600

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,12 +1,13 @@
-
-
 public class Card{
     private String[] nums = new String[] {"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"};
     private String[] suits = new String[] {"Spades", "Hearts", "Clubs", "Diamonds"};
     private int num;
     private int suit;
-    private Node next;
-    private Node card;
+
+    public Card() {
+        this.num = -1;
+        this.suit = -1;
+    }
 
     public Card(int num, int suit) {
 
@@ -19,36 +20,33 @@ public class Card{
         }
 
         else {
-            this.card = new Node(num, suit, null);
+            this.num = num;
+            this.suit = suit;
         }
 
     }//method Card
 
-    public Node getNextNode() {
-        return this.card.next;
-    }
-
     public int getCardNum() {
 
-        return this.card.num;
+        return this.num;
 
     }//getter for Card Num
 
     public String getNumName() {
 
-        return nums[this.card.num - 1];
+        return nums[this.num - 1];
 
     }//getter for Card Number Name
 
     public int getSuitNum() {
 
-        return this.card.suit;
+        return this.suit;
 
     }//getter Suit Num
 
     public String getSuitName() {
 
-        return suits[this.card.suit - 1];
+        return suits[this.suit - 1];
 
     }//getter for Suit Name
 
@@ -58,7 +56,7 @@ public class Card{
             throw new IllegalArgumentException("Suit must be between 1-4");
         }
         else {
-            this.card.suit = suit;
+            this.suit = suit;
         }
 
     }//setter for Suit
@@ -68,20 +66,7 @@ public class Card{
             throw new IllegalArgumentException("Rank must be 1 - 13");
         }
         else {
-            this.card.num = num;
-        }
-    }
-
-    private class Node {
-        private Node next;
-        private int num;
-        private int suit;
-
-        private Node(int num, int suit, Node next) {
             this.num = num;
-            this.suit = suit;
-            this.next = next;
         }
     }
-
 }//class Card
