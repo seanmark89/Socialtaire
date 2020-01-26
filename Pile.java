2c44830e490da93ab92498b5b977ae7459f91d98
oseli001 <60305674+oseli001@users.noreply.github.com>
2020-01-25 23:02:50 -0600

    Update Pile.java

--- Pile.java
+++ Pile.java
@@ -1,7 +1,22 @@
 
-public class Pile extends Card {
-  private Card topCard;
-  private int amount;
-  public Pile(Card top, int amount);
-  
-}  
+public class Pile extends Card{
+
+    private Card topCard;
+    private int amount;
+
+    public Pile(Card card, int amount) {
+        this.topCard = card;
+        this.amount = amount;
+    }
+
+    public void addCard(Card card) {
+        card.setNextNode(this.topCard);
+        this.topCard = card;
+        this.amount++;
+    }
+
+    public Card removeCard(Card card) {
+        this.topCard = card.getNextNode();
+    }
+}
+
